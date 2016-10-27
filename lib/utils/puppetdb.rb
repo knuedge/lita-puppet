9b5294f655f7a09440f45f36af4f796b0df85bf8
Jonathan Gnagy <jgnagy@knuedge.com>
2016-10-27 15:57:22 -0700

    Namespacing...

--- lib/utils/puppetdb.rb
+++ lib/utils/puppetdb.rb
@@ -3,14 +3,14 @@ module Utils
   module PuppetDB
     def dbquery(url, q)
       # TODO: validate incoming query structure
-      client = PuppetDB::Client.new(server: url)
+      client = ::PuppetDB::Client.new(server: url)
       client.request *q
     end
 
     def node_roles_and_profiles(url, nodename)
       # TODO: validate url and nodename
-      PuppetDB::Client.new(server: url) # this is weird but required
-      d = PuppetDB::Client.get("/catalogs/#{nodename}")
+      ::PuppetDB::Client.new(server: url) # this is weird but required
+      d = ::PuppetDB::Client.get("/catalogs/#{nodename}")
       return d["error"] if d.is_a?(Hash) && d['error']
 
       tags = []
